ede65781c3ffb4fe12a78cb3b2894615216f9c6f
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-07-30 14:38:23 +0200

    Initialisation du projet

---
+++ src/test/java/fr/quinzeminutespourmoi/quinzePourMoi/QuinzePourMoiApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.quinzeminutespourmoi.quinzePourMoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuinzePourMoiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
