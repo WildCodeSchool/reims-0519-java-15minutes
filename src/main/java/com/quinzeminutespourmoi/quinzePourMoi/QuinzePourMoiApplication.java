94ffb14bba1fcf96a0b8d4f0888358b5bf87f08c
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-07-31 16:07:19 +0200

    initialisation

---
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/QuinzePourMoiApplication.java
@@ -0,0 +1,13 @@
+package com.quinzeminutespourmoi.quinzePourMoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinzePourMoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinzePourMoiApplication.class, args);
+	}
+
+}
