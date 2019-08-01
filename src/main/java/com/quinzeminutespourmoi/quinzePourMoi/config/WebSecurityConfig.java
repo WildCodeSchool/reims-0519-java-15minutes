ddf3ae4b076d5055ac6d325ce345835a859af39a
cecil millerioux <cecil.millerioux@gmail.com>
2019-08-01 16:13:27 +0200

    added connection form

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/config/WebSecurityConfig.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.quinzeminutespourmoi.quinzePourMoi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,31 +20,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             return super.authenticationManagerBean();
     }
  
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-            .withUser("user1").password("{noop}user1Pass").roles("USER")
+        .withUser("user")
+            .password(encoder.encode("password"))
+            .roles("USER")
             .and()
-            .withUser("admin1").password("{noop}admin1Pass").roles("ADMIN");
+        .withUser("admin")
+            .password(encoder.encode("password"))
+            .roles("ADMIN");
     }
- 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/anonymous*").anonymous()
-            .antMatchers("/login*").permitAll()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/index").access("hasRole('USER') or hasRole('ADMIN')")
             .anyRequest().authenticated()
-             
             .and()
             .formLogin()
-            .loginPage("/login.html")
-            .loginProcessingUrl("/login")
-            .failureUrl("/login.html?error=true")
-             
-            .and()
-            .logout().deleteCookies("JSESSIONID")
-             
-            .and()
-            .rememberMe().key("uniqueAndSecret").tokenValiditySeconds(86400); // for one day in second            ;
+            .defaultSuccessUrl("/index", true);
     }
 }
