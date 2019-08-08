98612cacc11a2a9f922ccdca00962fd464b3b354
Vincent Guegan <gueganvincent@gmail.com>
2019-08-08 14:39:58 +0200

    initialization PageController

---
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/PageController.java
@@ -0,0 +1,15 @@
+package com.quinzeminutespourmoi.quinzePourMoi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class PageController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+}
