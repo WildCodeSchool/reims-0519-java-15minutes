5ca4de8da6829b2abb98060ca2b8f4ed7a76b1f0
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-08-12 16:15:29 +0200

    Creation list praticiens html

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/PageController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/PageController.java
@@ -9,37 +9,7 @@ class PageController {
 
     @GetMapping("/")
     public String index(Model model) {
-        return "index";
+        return "intro";
     }
-
-    @GetMapping("/about-us")
-    public String aboutUs(Model model) {
-        return "about-us";
-    }
-
-    @GetMapping("/questions")
-    public String prepaQuest(@RequestParam Integer page, Model model) {
-        if(page) { // sous-entendu page != null
-            if(page.equals(1)) {
-                return "choices";
-            }
-            if(page.equals(2)) {
-                return "beforeQuest";
-            }
-            if(page.equals(3)) {
-                return "information";
-            }
-            if(page.equals(4)) {
-                return "intro";
-            }
-            // les pages 2, 3... seront à rajouter ici
-            if(page.equals(100)) {
-                return "result";
-            }
-            if(page.equals(101)) {
-                return "resultQuest";
-            }
-        }
-        return "prepaQuest"
-    }
+    
 }
