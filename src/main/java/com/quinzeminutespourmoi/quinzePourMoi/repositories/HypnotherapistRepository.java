171862172e571d839fd42d682c6752b1b61d13ab
cecil millerioux <cecil.millerioux@gmail.com>
2019-08-14 10:03:09 +0200

    rename pratitioner repo on hypnotherpist repo

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/repositories/HypnotherapistRepository.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/repositories/HypnotherapistRepository.java
@@ -2,8 +2,9 @@ package com.quinzeminutespourmoi.quinzePourMoi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.quinzeminutespourmoi.quinzePourMoi.entities.Practitioner;
+
+import com.quinzeminutespourmoi.quinzePourMoi.entities.Hypnotherapist;
 
 @Repository
-public interface PractitionerRepository extends JpaRepository<Practitioner, Long> {
+public interface HypnotherapistRepository extends JpaRepository<Hypnotherapist, Long> {
 }
