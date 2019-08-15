cd4903b841dfefaae9d4067ab67270b36a74dce4
cecil millerioux <cecil.millerioux@gmail.com>
2019-08-15 10:14:13 +0200

    delete of username champ

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/entities/User.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/entities/User.java
@@ -11,10 +11,9 @@ public class User {
     public User() {
     }
 
-    public User(String firstname, String lastname, String username, String password, String mail, String role){
+    public User(String firstname, String lastname, String password, String mail, String role){
         this.firstname = firstname;
         this.lastname = lastname;
-        this.username = username;
         this.password = password;
         this.mail = mail;
         this.role = role;
@@ -25,7 +24,6 @@ public class User {
     private Long id;
     private String firstname;
     private String lastname;
-    private String username;
     private String password;
     private String mail;
     private String role;
@@ -36,14 +34,6 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
