acb81f0e50e24dee5f1fab9d53db69a08217cd62
Cécil Millerioux <45787983+cecilmillerioux@users.noreply.github.com>
2019-08-29 17:31:37 +0200

    Update Notification.java

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/entities/Notification.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/entities/Notification.java
@@ -11,16 +11,16 @@ public class Notification{
     public Notification() {
     }
 
-    public Notification(Long hypnotherapist_id, Long user_id) {
-        this.hypnotherapist_id = hypnotherapist_id;
-        this.user_id = user_id;
+    public Notification(Long hypnotherapistId, Long userId) {
+        this.hypnotherapistId = hypnotherapistId;
+        this.userId = userId;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long hypnotherapist_id;
-    private Long user_id;
+    private Long hypnotherapistId;
+    private Long userId;
 
     public Long getId() {
         return id;
@@ -30,19 +30,19 @@ public class Notification{
         this.id = id;
     }
 
-    public Long getHypnotherapist_id() {
-        return hypnotherapist_id;
+    public Long getHypnotherapistId() {
+        return hypnotherapistId;
     }
 
-    public void setHypnotherapist_id(Long hypnotherapist_id) {
-        this.hypnotherapist_id = hypnotherapist_id;
+    public void setHypnotherapistId(Long hypnotherapistId) {
+        this.hypnotherapistId = hypnotherapistId;
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public Long getUserId() {
+        return userId;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
