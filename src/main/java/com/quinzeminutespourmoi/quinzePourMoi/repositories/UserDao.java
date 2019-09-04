f623a51a62f8a7f3133f1232e06734dbba245154
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-09-04 14:41:08 +0200

    create JPA hypnotherapiste

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/repositories/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.quinzeminutespourmoi.quinzePourMoi.repositories;
-
-import com.quinzeminutespourmoi.quinzePourMoi.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends JpaRepository<User, Long> {
-}
