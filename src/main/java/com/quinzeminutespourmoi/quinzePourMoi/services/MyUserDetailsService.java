0a995416b3ef450de6fb343492bd1d2aa5364561
cecil <cecil.millerioux@gmail.com>
2019-09-04 21:05:02 +0200

    correction on MyUserDetailsService

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/services/MyUserDetailsService.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/services/MyUserDetailsService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
