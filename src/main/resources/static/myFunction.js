48c7981b5db1144345ab5837661a1ac1301d5fe0
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-05 16:09:48 +0200

    add function js for register form and re add bg-light on subscribe template

---
+++ src/main/resources/static/myFunction.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
