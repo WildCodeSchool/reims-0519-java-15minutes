dd10dcfcddf674f679858156934f8e795edebcb7
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-05 10:40:28 +0200

    resolve conflict for pull dev

---
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/HypnotherapistController.java
@@ -0,0 +1,28 @@
+package com.quinzeminutespourmoi.quinzePourMoi.controllers;
+
+import com.quinzeminutespourmoi.quinzePourMoi.repositories.HypnotherapistRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class HypnotherapistController {
+
+    @Autowired
+    private HypnotherapistRepository hypnotherapistRepository;
+    
+    @GetMapping("/hypnotherapists") 
+    public String hypnotherapists(Model model){
+        model.addAttribute(
+            "hypnotherapists",
+            hypnotherapistRepository.findAll()
+        );
+        return "hypnotherapists";
+    }
+    @GetMapping("/infos")
+    public String infos(Model model) {
+        return "infos";
+    }
+}
