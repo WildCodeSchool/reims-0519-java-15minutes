027da0e2679833daecb9050df62d72c16e44bf03
Lea Goarant <leagoarant@msn.com>
2019-09-11 15:01:36 +0200

    ajout des formulaires de peur et des routes

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/SurveyController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/SurveyController.java
@@ -9,6 +9,56 @@ class SurveyController{
 
     @GetMapping("/smokeSurvey")
     public String smokesurvey(Model model){
-        return "smokesurvey";
+        return "smokeSurvey";
+    }
+
+    @GetMapping("/addictSurvey")
+    public String addictsurvey(Model model){
+        return "addictSurvey";
+    }
+
+    @GetMapping("/foodAddictionSurvey")
+    public String foodAddictionSurvey(Model model){
+        return "foodAddictionSurvey";
+    }
+
+    @GetMapping("/screenAddictionSurvey")
+    public String screenAddictionSurvey(Model model){
+        return "screenAddictionSurvey";
+    }
+
+    @GetMapping("/alcoolAddictionSurvey")
+    public String alcoolAddictionSurvey(Model model){
+        return "alcoolAddictionSurvey";
+    }
+
+    @GetMapping("/animalsFear")
+    public String animalsFear(Model model){
+        return "animalsFear";
+    }
+
+    @GetMapping("/carFear")
+    public String carFear(Model model){
+        return "carFear";
+    }
+
+    @GetMapping("/planeFear")
+    public String planeFear(Model model){
+        return "planeFear";
+    }
+
+    @GetMapping("/peopleFear")
+    public String peopleFear(Model model){
+        return "peopleFear";
+    }
+
+    @GetMapping("/waterFear")
+    public String waterFear(Model model){
+        return "waterFear";
+    }
+
+    @GetMapping("/closeFear")
+    public String closeFear(Model model){
+        return "closeFear";
     }
 }
