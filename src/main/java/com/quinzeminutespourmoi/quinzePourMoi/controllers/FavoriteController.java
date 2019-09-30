938d6de2697efc29d6ba39178c36b3cd57bef905
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-30 18:35:19 +0200

    add condition and delete function but, th:if condition for favorite != null do not work

---
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/FavoriteController.java
@@ -0,0 +1,33 @@
+package com.quinzeminutespourmoi.quinzePourMoi.controllers;
+
+import com.quinzeminutespourmoi.quinzePourMoi.entities.Favorite;
+import com.quinzeminutespourmoi.quinzePourMoi.entities.User;
+import com.quinzeminutespourmoi.quinzePourMoi.repositories.FavoriteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+class FavoriteController {
+
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+
+    @PostMapping("/favorites")
+    public String store(Authentication authentication, Favorite favorite){
+        favorite.setUser((User)authentication.getPrincipal());
+        favorite = favoriteRepository.save(favorite);
+        return "redirect:/hypnotherapists/"+favorite.getHypnotherapist().getId();
+    }
+
+    @DeleteMapping("/favorites/{id}")
+    public String destroy(@PathVariable Long id, Favorite favorite) {
+        favoriteRepository.delete(
+            favoriteRepository.findById(id).get());
+        return "redirect:/hypnotherapists/"+favorite.getHypnotherapist().getId();
+    }
+}
