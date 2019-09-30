9e4ec8c33dccf3c1735dacbe8e9b5eef612f5685
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-09-30 15:49:40 +0200

    clean code

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/HypnotherapistController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/HypnotherapistController.java
@@ -17,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
