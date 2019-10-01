79d270369413e96d644a35071a999d9a8ebf118f
cecil millerioux <cecil.millerioux@gmail.com>
2019-10-01 14:16:12 +0200

    set the correct condition for no favorite display

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/FavoriteController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/FavoriteController.java
@@ -1,20 +1,12 @@
 package com.quinzeminutespourmoi.quinzePourMoi.controllers;
 
-import java.util.List;
-
 import com.quinzeminutespourmoi.quinzePourMoi.entities.Favorite;
-import com.quinzeminutespourmoi.quinzePourMoi.entities.Notification;
 import com.quinzeminutespourmoi.quinzePourMoi.entities.User;
 import com.quinzeminutespourmoi.quinzePourMoi.repositories.FavoriteRepository;
-import com.quinzeminutespourmoi.quinzePourMoi.repositories.NotificationRepository;
-import com.quinzeminutespourmoi.quinzePourMoi.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,25 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 class FavoriteController {
 
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private NotificationRepository notificationRepository;
-    @Autowired
     private FavoriteRepository favoriteRepository;
-
-    @GetMapping("/favorites")
-    public String favorites(Model model, Authentication authentication, Long hypnotherapistId) {
-        if(authentication != null){
-            User user = userRepository.findByMail(authentication.getName());
-            model.addAttribute("user", user);
-            Notification notification = notificationRepository.findNotificationByUserIdOrHypnotherapistId(user.getId(), user.getId());
-            model.addAttribute("notification", notification);
-        }
-        User user = (User)authentication.getPrincipal();
-        List <Favorite> favorites = favoriteRepository.findFavoriteByUserIdOrHypnotherapistId(user.getId(),user.getId());
-        model.addAttribute("favorites", favorites);
-        return "favorites";
-    }
 
     @PostMapping("/favorites")
     public String store(Authentication authentication, Favorite favorite){
