2dbeeb174e97e668d647c93c29eac0645868e32d
Lea Goarant <leagoarant@msn.com>
2019-10-01 11:29:44 +0200

    ajout mapping

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/RateController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/RateController.java
@@ -5,6 +5,8 @@ import com.quinzeminutespourmoi.quinzePourMoi.repositories.RateRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
