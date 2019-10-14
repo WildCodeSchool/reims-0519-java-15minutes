d73a9c21b13ed18d5d6c7b2b0eefffe4b088dd15
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-10-14 11:03:25 +0200

    change margin on profil user

--- src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/LoginController.java
+++ src/main/java/com/quinzeminutespourmoi/quinzePourMoi/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.quinzeminutespourmoi.quinzePourMoi.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController{
@@ -13,5 +16,15 @@ public class LoginController{
     @GetMapping("/loginError")
     public String loginError() {
         return "/loginError";
+    }
+    @Controller
+    public class DefaultController {
+        @RequestMapping("/default")
+        public String defaultAfterLogin(HttpServletRequest request) {
+            if (request.isUserInRole("Hypnotherapist")) {
+                return "redirect:/users/profil";
+            }
+            return "redirect:/home/";
+        }
     }
 }
